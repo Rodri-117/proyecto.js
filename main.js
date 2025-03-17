16ca5ae1ecd6a94b91b1e513d30c211d3352c847
Rodri-117 <rodrifaccini@gmail.com>
2025-03-17 02:25:10 -0300

    entrega2

--- main.js
+++ main.js
@@ -1,3 +1,47 @@
+
+    class Prenda {
+        constructor (id, nombre, precio, talle) {
+            this.id = id;
+            this.nombre = nombre;
+            this.precio = precio;
+            this.talle = talle;
+        }  
+    }
+
+    const inventario = [
+        new Prenda(111, "Pantalon", 20000, "S"),
+        new Prenda(222, "Remera", 8000, "M"),
+        new Prenda(333, "Sudadera", 12000, "M"),
+        new Prenda(444, "Campera", 25000, "L"),
+    ];
+    
+    
+    function filtrarPrendas(){
+        let palabraClave = prompt ("buscar").trim().toUpperCase();
+        let resultado = []
+
+        resultado = inventario.filter((x) => x.nombre.toUpperCase() === palabraClave);
+        if (resultado.length === 0) {
+            let monto = Number(palabraClave);
+            if (!isNaN(monto)) {
+                resultado = inventario.filter(prenda => prenda.precio <= monto);
+            }
+        }
+        if (palabraClave === "S" || palabraClave === "M" || palabraClave === "L" || palabraClave === "XL") {
+            resultado = inventario.filter(prenda => prenda.talle.toUpperCase() === palabraClave);
+        }        
+        if (resultado.length > 0) {
+            console.log("Resultados encontrados:", resultado);
+        } else {
+            alert("No se encontraron coincidencias.");
+        }
+    }
+
+    filtrarPrendas();
+
+    /*
+    
+    
     function tiendaOnline () {
         
         
@@ -33,4 +77,4 @@
 
     tiendaOnline();
 
-    
+    */
